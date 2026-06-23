58e77c65c69a97b74e7ed2ef184bbc8b9d13a6bd
coder-aryan14 <aryankhedulkar@gmail.com>
2026-06-23 15:06:27 +0530

    feat: implement profile management

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,3 +2,4 @@ export * from "./client";
 export * from "./schema";
 export * from "./services/creator-profiles";
 export * from "./services/create-creator-profiles";
+export * from "./services/update-creator-profile";
